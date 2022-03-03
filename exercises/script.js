fdaa7ae6352edb98d346d211e2b59a6d388a7f11
edzer <jeddionido26@gmail.com>
2022-03-03 11:38:49 +0800

    add code to js file

--- exercises/script.js
+++ exercises/script.js
@@ -1,32 +1,38 @@
 let movies = [{
-  title: "Train to Busan",
-  rating: 4.5,
-  hasWatched: true
-}, 
+    title: "Spider-Man: No Way Home",
+    rating: 3.5,
+    hasWatched: true
+  },
 
-{
-  title: "Encanto",
-  rating: 5,
-  hasWatched: false
-},
+  {
+    title: "Unbroken",
+    rating: 5,
+    hasWatched: true
+  },
 
-{
-  title: "Die Hard",
-  rating: 4.6,
-  hasWatched: false
-}, 
-  
-{
-  title: "Spider-Man: No Way Home",
-  rating: 5,
-  hasWatched: true
-}];
+  {
+    title: "Frozen 2",
+    rating: 5,
+    hasWatched: false
+  },
+
+  {
+    title: "Encanto",
+    rating: 5,
+    hasWatched: false
+  },
+
+  {
+    title: "Forrest Gump",
+    rating: 5,
+    hasWatched: true
+  }
+];
 
 for (let i = 0; movies.length > i; i++) {
-    if(movies[i].hasWatched === true) {
-      document.write("<h1> You have watched " + '"' + movies[i].title + '"' + " - " + movies[i].rating + " stars" + "<br> </h1>")
-    } else {
-      document.write("<h2> You have not seen " + '"' + movies[i].title + '"' + " - " + movies[i].rating + " stars" + "<br> </h2>")
-    }
+  if (movies[i].hasWatched === true) {
+    document.write("<p> You have watched " + '"' + movies[i].title + '"' + " - " + movies[i].rating + " stars <br> </p>");
+  } else {
+    document.write("<p>You have not seen " + '"' + movies[i].title + '"' + " - " + movies[i].rating + " stars <br> </p>");
+  }
 }
-
